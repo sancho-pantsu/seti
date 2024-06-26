79c45aa15fc41e46e007daffee226dd648f57aeb
shishkin.ae <shishkin.ae@skbkontur.ru>
2024-06-26 17:59:58 +0500

    fake

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import json
-import random
 
 app = Flask(__name__)
 
@@ -52,6 +51,11 @@ def save_progress(progress):
         json.dump(progress, f)
 
 
+def reset_progress():
+    if os.path.exists(PROGRESS_FILE):
+        os.remove(PROGRESS_FILE)
+
+
 questions_data = load_questions()
 progress_data = load_progress()
 
@@ -67,39 +71,38 @@ def test(test_name):
         return "Test not found", 404
     questions = list(questions_data[test_name].items())
     first_question_id = questions[0][0]
-    first_question = questions_data[test_name][first_question_id]
-    return render_template('question.html', test_name=test_name, question=first_question, question_id=first_question_id)
+    return redirect(url_for('question', test_name=test_name, question_id=first_question_id))
 
 
-@app.route('/question/<test_name>/<question_id>')
+@app.route('/question/<test_name>/<question_id>', methods=['GET', 'POST'])
 def question(test_name, question_id):
     if test_name not in questions_data or question_id not in questions_data[test_name]:
         return "Question not found", 404
+
     question = questions_data[test_name][question_id]
-    return render_template('question.html', test_name=test_name, question=question, question_id=question_id)
+    if request.method == 'POST':
+        correct_answers = question['correct']
+        answers = request.form.getlist('answers')
+        if not answers:
+            answers = [request.form.get('answer')]
 
-
-@app.route('/submit_answer/<test_name>/<question_id>', methods=['POST'])
-def submit_answer(test_name, question_id):
-    if test_name not in questions_data or question_id not in questions_data[test_name]:
-        return "Question not found", 404
-    answers = request.form.getlist('answers')
-    correct_answers = questions_data[test_name][question_id]['correct']
-    is_correct = all(answer in correct_answers for answer in answers) and len(answers) == len(correct_answers)
-    progress_data[test_name] = progress_data.get(test_name, {})
-    progress_data[test_name][question_id] = (answers, is_correct)
-    save_progress(progress_data)
+        is_correct = all(answer in correct_answers for answer in answers) and len(answers) == len(correct_answers)
+        progress_data[test_name] = progress_data.get(test_name, {})
+        progress_data[test_name][question_id] = (answers, is_correct)
+        save_progress(progress_data)
 
     next_question_id = None
+    prev_question_id = None
     question_ids = list(questions_data[test_name].keys())
     current_index = question_ids.index(question_id)
     if current_index < len(question_ids) - 1:
         next_question_id = question_ids[current_index + 1]
+    if current_index > 0:
+        prev_question_id = question_ids[current_index - 1]
 
-    if next_question_id:
-        return redirect(url_for('question', test_name=test_name, question_id=next_question_id))
-    else:
-        return redirect(url_for('result', test_name=test_name))
+    result = progress_data[test_name].get(question_id) if test_name in progress_data else None
+    return render_template('question.html', test_name=test_name, question=question, question_id=question_id,
+                           next_question_id=next_question_id, prev_question_id=prev_question_id, result=result)
 
 
 @app.route('/result/<test_name>')
@@ -108,6 +111,12 @@ def result(test_name):
         return "No progress found for this test", 404
     results = progress_data[test_name]
     return render_template('result.html', test_name=test_name, results=results, questions=questions_data[test_name])
+
+
+@app.route('/reset/<test_name>')
+def reset(test_name):
+    reset_progress()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
